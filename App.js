84d52da561c8f3074ce4802f05a4bdd82327339a
maraBOOT <doueameaud13@gmail.com>
2023-02-07 18:49:10 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button,SafeAreaView } from 'react-native';
 import * as Contacts from 'expo-contacts';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 export default function App() {
 
+  let messages = [
+      {
+        header:"Côte d'Ivoire",
+        body:"Le père Daloa yako, c'est ton nom qui est gâté oh sinon les vraies mendiantes sont là. Oh wonte !"
+      },
+      {
+        header:"Roi 12 12",
+        body:"C'est ton fils Pantcho qui avait raison."
+      },
+      {
+        header:"Humm femme !",
+        body:"Elles vont venir mentir maintenant, cô c'était pour bader."
+      },
+      {
+        header:"Eh Dieu,",
+        body:"Donc dans amusément là, 2 de mes gos sont dedans 😓."
+      },
+      {
+        header:"Sarah,",
+        body:"Si je n'ai pas dit à ta maman la faut changer mon nom ☝️."
+      },
+      {
+        header:"J'ai vu",
+        body:"L'une des gos là dans mon secteur, elle a nuque hein."
+      },
+      {
+        header:"Je parie",
+        body:"Qu'elles sont toutes Dioula. Leur affaire de mendier !"
+      },
+      {
+        header:"On dit",
+        body:"Qu'elles sont les tantes des petits de la 2 qui demandent l'argent aux passants  là."
+      },
+      {
+        header:"Mon ex",
+        body:"Est dedans oh. Elle habite à Abobo. "
+      },
+      {
+        header:"Poutchoulou",
+        body:"C'est l'homme hein. "
+      },
+  ];
+  function getRandomInt() {
+    let max = messages.length;
+    return Math.floor(Math.random() * max);
+  }
+  const [random,setRandom]=useState(getRandomInt());
   useEffect(() => {
     (async () => {
       const { status } = await Contacts.requestPermissionsAsync();
@@ -14,7 +61,7 @@ export default function App() {
 
         if (data.length > 0) {
           const contact = data[0];
-          console.log(data.length,contact.phoneNumbers);
+          console.log(data.length, contact.phoneNumbers);
         }
       }
     })();
@@ -25,25 +72,66 @@ export default function App() {
 
   };
   return (
-    <View style={styles.container}>
-      <Text>Cliquez pour verifier s'il y'a des mendiantes parmi vos contacts
+    <SafeAreaView style={styles.container}>
+      
+      <View style={styles.messageContainer}>
+        <Text style={styles.header}>{messages[random].header}</Text>
+        <Text style={styles.message}>{messages[random].body}
+        </Text>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Button
+          onPress={buttonClick}
+          title="Rechercher"
+          color="#841584"
+          accessibilityLabel=""
+        />
+      </View>
+
+      <Text style={styles.powerBy}>Powered by Ghostscripter
       </Text>
-      <Button
-        onPress={buttonClick}
-        title="Rechercher"
-        color="#841584"
-        accessibilityLabel=""
-      />
+
       {/* <StatusBar style="auto" /> */}
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
+  },
+  info:{
+    backgroundColor:"red",
+    
+  },
+  header:{
+    fontSize: 41,
+    fontWeight: "900",
+    color: "#999"
+  },
+  message: {
+    fontSize: 43,
+    fontWeight: "900",
+    color: "#fff"
+  },
+  messageContainer: {
+    flex: 3,
+    //backgroundColor: 'red',
+    //alignItems: 'center',
     justifyContent: 'center',
+  },
+  btnContainer:{
+    flex:1
+  },
+  powerBy: {
+    //backgroundColor: 'blue',
+    color: "#444",
+    //alignItems: 'center',
+    justifyContent: "flex-end",
+    //alignSelf: "flex-end",
+    marginBottom:5
   },
 });
