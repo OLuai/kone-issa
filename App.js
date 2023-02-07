2a64eed1cc22bb775b8d0424ab8000f7f6592b3c
OLuai <ouragaluai@yahoo.com>
2023-02-07 16:57:30 +0000

    +init

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import { useEffect } from 'react';
 export default function App() {
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails, , Contacts.Fields.PhoneNumbers],
+        });
+
+        if (data.length > 0) {
+          const contact = data[0];
+          console.log(data.length,contact.phoneNumbers);
+        }
+      }
+    })();
+  }, []);
+
+  //Fonction qui est appellee lorsqu'on clique sur le bouton
+  const buttonClick = () => {
+
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Cliquez pour verifier s'il y'a des mendiantes parmi vos contacts
+      </Text>
+      <Button
+        onPress={buttonClick}
+        title="Rechercher"
+        color="#841584"
+        accessibilityLabel=""
+      />
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
