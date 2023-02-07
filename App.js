1640d78b5384d327436d50fefa9d60f3b9a38196
OLuai <ouragaluai@yahoo.com>
2023-02-07 19:21:55 +0000

    ++ helpers

--- App.js
+++ App.js
@@ -1,84 +1,97 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button,SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, ActivityIndicator } from 'react-native';
 import * as Contacts from 'expo-contacts';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
+import { checkBeggars } from './Helpers';
 export default function App() {
 
   let messages = [
-      {
-        header:"Côte d'Ivoire",
-        body:"Le père Daloa yako, c'est ton nom qui est gâté oh sinon les vraies mendiantes sont là. Oh wonte !"
-      },
-      {
-        header:"Roi 12 12",
-        body:"C'est ton fils Pantcho qui avait raison."
-      },
-      {
-        header:"Humm femme !",
-        body:"Elles vont venir mentir maintenant, cô c'était pour bader."
-      },
-      {
-        header:"Eh Dieu,",
-        body:"Donc dans amusément là, 2 de mes gos sont dedans 😓."
-      },
-      {
-        header:"Sarah,",
-        body:"Si je n'ai pas dit à ta maman la faut changer mon nom ☝️."
-      },
-      {
-        header:"J'ai vu",
-        body:"L'une des gos là dans mon secteur, elle a nuque hein."
-      },
-      {
-        header:"Je parie",
-        body:"Qu'elles sont toutes Dioula. Leur affaire de mendier !"
-      },
-      {
-        header:"On dit",
-        body:"Qu'elles sont les tantes des petits de la 2 qui demandent l'argent aux passants  là."
-      },
-      {
-        header:"Mon ex",
-        body:"Est dedans oh. Elle habite à Abobo. "
-      },
-      {
-        header:"Poutchoulou",
-        body:"C'est l'homme hein. "
-      },
+    {
+      header: "Côte d'Ivoire",
+      body: "Le père Daloa yako, c'est ton nom qui est gâté oh sinon les vraies mendiantes sont là. Oh wonte !"
+    },
+    {
+      header: "Roi 12 12",
+      body: "C'est ton fils Pantcho qui avait raison."
+    },
+    {
+      header: "Humm femme !",
+      body: "Elles vont venir mentir maintenant, cô c'était pour bader."
+    },
+    {
+      header: "Eh Dieu,",
+      body: "Donc dans amusément là, 2 de mes gos sont dedans 😓."
+    },
+    {
+      header: "Sarah,",
+      body: "Si je n'ai pas dit à ta maman la faut changer mon nom ☝️."
+    },
+    {
+      header: "J'ai vu",
+      body: "L'une des gos là dans mon secteur, elle a nuque hein."
+    },
+    {
+      header: "Je parie",
+      body: "Qu'elles sont toutes Dioula. Leur affaire de mendier !"
+    },
+    {
+      header: "On dit",
+      body: "Qu'elles sont les tantes des petits de la 2 qui demandent l'argent aux passants  là."
+    },
+    {
+      header: "Mon ex",
+      body: "Est dedans oh. Elle habite à Abobo. "
+    },
+    {
+      header: "Poutchoulou",
+      body: "C'est l'homme hein. "
+    },
   ];
   function getRandomInt() {
     let max = messages.length;
     return Math.floor(Math.random() * max);
   }
-  const [random,setRandom]=useState(getRandomInt());
-  useEffect(() => {
-    (async () => {
-      const { status } = await Contacts.requestPermissionsAsync();
-      if (status === 'granted') {
-        const { data } = await Contacts.getContactsAsync({
-          fields: [Contacts.Fields.Emails, , Contacts.Fields.PhoneNumbers],
-        });
+  const [random, setRandom] = useState(getRandomInt());
+  const [indicator, setIndicator] = useState(false);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await Contacts.requestPermissionsAsync();
+  //     if (status === 'granted') {
+  //       const { data } = await Contacts.getContactsAsync({
+  //         fields: [Contacts.Fields.Emails, , Contacts.Fields.PhoneNumbers],
+  //       });
 
-        if (data.length > 0) {
-          const contact = data[0];
-          console.log(data.length, contact.phoneNumbers);
-        }
-      }
-    })();
-  }, []);
+  //       if (data.length > 0) {
+  //         const contact = data[0];
+  //         console.log(data.length, contact.phoneNumbers);
+  //       }
+  //     }
+  //   })();
+  // }, []);
 
   //Fonction qui est appellee lorsqu'on clique sur le bouton
   const buttonClick = () => {
-
+    setIndicator(true);
+    checkBeggars().then(result => {
+      console.log('result ----->', result);
+      setRandom(getRandomInt);
+    }).finally(() => {
+      setTimeout(() => {
+        setIndicator(false);
+      }, 2000)
+      // setIndicator(false);
+    });
   };
   return (
     <SafeAreaView style={styles.container}>
-      
+
       <View style={styles.messageContainer}>
         <Text style={styles.header}>{messages[random].header}</Text>
         <Text style={styles.message}>{messages[random].body}
         </Text>
       </View>
+
+      <ActivityIndicator style={{ marginBottom: 20 }} size="large" color="#841584" animating={indicator} />
 
       <View style={styles.btnContainer}>
         <Button
@@ -89,7 +102,7 @@ export default function App() {
         />
       </View>
 
-      <Text style={styles.powerBy}>Powered by Ghostscripter
+      <Text style={styles.powerBy}>Powered by maraBOOT & GhostScripter
       </Text>
 
       {/* <StatusBar style="auto" /> */}
@@ -103,11 +116,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     alignItems: 'center',
   },
-  info:{
-    backgroundColor:"red",
-    
+  info: {
+    backgroundColor: "red",
+
   },
-  header:{
+  header: {
     fontSize: 41,
     fontWeight: "900",
     color: "#999"
@@ -123,8 +136,8 @@ const styles = StyleSheet.create({
     //alignItems: 'center',
     justifyContent: 'center',
   },
-  btnContainer:{
-    flex:1
+  btnContainer: {
+    flex: 1
   },
   powerBy: {
     //backgroundColor: 'blue',
@@ -132,6 +145,6 @@ const styles = StyleSheet.create({
     //alignItems: 'center',
     justifyContent: "flex-end",
     //alignSelf: "flex-end",
-    marginBottom:5
+    marginBottom: 5
   },
 });
