e467f50abc3dc5e76a27b1dad014b9a0ce51020d
maraBOOT <doueameaud13@gmail.com>
2023-02-07 20:57:26 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button,SafeAreaView,Pressable} from 'react-native';
+import { StyleSheet, Text, View, Button,SafeAreaView,Pressable,ActivityIndicator} from 'react-native';
 import * as Contacts from 'expo-contacts';
 import { useEffect,useState } from 'react';
 import Modal from "react-native-modal";
